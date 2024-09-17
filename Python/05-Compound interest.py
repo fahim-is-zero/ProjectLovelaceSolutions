e12ae826f7f252e686cf1cdc2c358ab7981d6ab3
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-18 00:28:28 +0600

    Update 05-Compound interest.py

--- Python/05-Compound interest.py
+++ Python/05-Compound interest.py
@@ -1,8 +1,12 @@
+# code that computes the final amount M after starting with an amount m compounded yearly at a r rate for n years.
+
 def compound_interest(amount, rate, years):
-    m = amount
+    m = amount  # starting amount
     r = rate
     n = years
 
-    M = m * (1 + r)** n
+    M = m * (1 + r)** n  # Formula of compound interest
 
     return M
+
+print(compound_interest(1000, 0.07 ,25))
