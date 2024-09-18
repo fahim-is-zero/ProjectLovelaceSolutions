8351535068e06fa789e1d5632ed5246c427677a9
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-19 00:11:37 +0600

    Create 08-Almost π.py

---
+++ Python/08-Almost π.py
@@ -0,0 +1,18 @@
+def almost_pi(N):
+    t = 0  # temp variable
+    n = 1
+
+    for i in range(1, (2 * N - 1) + 1, 2):  # generates 'N' - numbers odd numbers 
+        p = 1/(i)
+        if n % 2 == 0:
+            t -= p
+            n =+ 1
+        else:
+            t += p
+            n += 1
+    
+    pi = 4 * t  
+
+    return pi
+
+print(almost_pi(10000000))  
