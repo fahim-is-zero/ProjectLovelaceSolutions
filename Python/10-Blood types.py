af3020e3be4676239834453d1755dd0450984163
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-22 00:14:50 +0600

    Create 10-Blood types.py

---
+++ Python/10-Blood types.py
@@ -0,0 +1,19 @@
+donor = {"O-" : ['O-'],
+         "O+" : ['O+', 'O-'],
+         "A-" : ['A-', 'O-'],
+         "A+" : ['A+', 'A-', 'O+', 'O-'],
+         "B-" : ['O-', 'B-'],
+         "B+" : ['O+', 'O-', 'B+', 'B-'],
+         "AB-" : ['O-', 'AB-', 'A-', 'B-'],
+         "AB+" : ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']}
+
+
+def survive(blood_type, donated_blood):
+    needed_blood = donor[blood_type]
+    found_match = False
+    for blood in donated_blood:
+        if blood in needed_blood:
+            found_match = True
+            break
+    return found_match
+  
