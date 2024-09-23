efafa4c58d8ce19c1ea091a82adf8c1c397ac7bd
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-23 16:02:58 +0600

    Update 13-Rocket science.py

--- Python/13-Rocket science.py
+++ Python/13-Rocket science.py
@@ -1,11 +1,12 @@
-from math import e )
+from math import e 
 
-v_e = 2550  
-M = 250000  
+v_e = 2550  # rocket exhaust velocity [m/s]
+M = 250000  # Saturn V rocket dry mass [kg]
 
-def rocket_fuel(v):
+def rocket_fuel(v):  # v is planet escape velocity
 
-    m_fuel = M * (e**( v/v_e ) - 1)
+    m_fuel = M * (e**( v/v_e ) - 1)  
 
     return m_fuel
 
+print("Earth : ", rocket_fuel(11186))  
