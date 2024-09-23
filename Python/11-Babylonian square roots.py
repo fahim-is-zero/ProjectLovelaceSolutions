30c402cf4485ebfc73fa4bce73ac6b628b72d951
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-23 16:00:54 +0600

    Update 11-Babylonian square roots.py

--- Python/11-Babylonian square roots.py
+++ Python/11-Babylonian square roots.py
@@ -1,11 +1,19 @@
+# Input: A number S.
+# Output: The square root of S approximated using the babylonian method. Answer should be accurate to 10 significant figures. That is, it should be correct to within 1 part in 10 billion, or the relative error should be lower than 10^-10. 
+
+
+
+
 def babylonian_sqrt(S):
     
-    x_n = S / 2 
+    x_n = S / 2  # initial guess
 
     if S == 0:
         return 0
     else:
         for i in range(0, 100):
-            x_n = 1/2 * (x_n + S / x_n) 
+            x_n = 1/2 * (x_n + S / x_n) # formula (Babylonian method)
 
     return x_n
+
+print(babylonian_sqrt(150626.35019758446 ))
