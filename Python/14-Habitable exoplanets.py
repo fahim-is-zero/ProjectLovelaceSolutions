1e7465e60003d59ffc6bedee6fd56fc751da0f99
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-24 17:45:08 +0600

    Update 14-Habitable exoplanets.py

--- Python/14-Habitable exoplanets.py
+++ Python/14-Habitable exoplanets.py
@@ -1,9 +1,14 @@
 # Circumstellar Habitable Zone (CHZ)
 
-def habitable_exoplanet(L, r):  
+# Input: The star's absolute luminosity L and the planet's distance from the star r.
+# Output: The string "too cold" if the planet is too far away, "too hot" if it is too close, or "just right" if it is within the CHZ. 
 
-    r_i = (L/1.1)**2   # inner radius of CHZ Star
-    r_o = (L/0.54)**2   # outer radius of CHZ Star
+from math import sqrt
+
+def habitable_exoplanet(L, r):   # L = Star's absolute luminosity, r =  The planet's distance from the star
+
+    r_i = sqrt(L/1.1)   # inner radius of CHZ Star (Unit : astroastronomical units(au))
+    r_o = sqrt(L/0.54)   # outer radius of CHZ Star (Unit : astroastronomical units(au))
 
     if r_i < r < r_o:
         return "just right"
