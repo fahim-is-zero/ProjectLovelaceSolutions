890f8e4e81cd9f7b583983ff69c2e5e843627fa9
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-26 00:09:19 +0600

    Create 18-Colorful resistors.py

---
+++ Python/18-Colorful resistors.py
@@ -0,0 +1,28 @@
+digits = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9
+}
+
+multiplier = {
+    'pink': 0.001,
+    'silver': 0.01,
+    'gold': 0.1,
+    'black': 1,
+    'brown': 10,
+    'red': 100,
+    'orange': 10 ** 3,
+    'yellow': 10 ** 4,
+    'green': 10 ** 5,
+    'blue': 10 ** 6,
+    'violet': 10 ** 7,
+    'grey': 10 ** 8,
+    'white': 10 ** 9
+}
