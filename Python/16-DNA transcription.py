c652e01900af81501aa75fcddb1ba14757a529b4
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-25 06:42:25 +0600

    Update 16-DNA transcription.py

--- Python/16-DNA transcription.py
+++ Python/16-DNA transcription.py
@@ -1,5 +1,11 @@
+# Input: A string of ATCG characters representing a DNA sequence.
+# Output: The RNA sequence corresponding to the input DNA sequence.
+
+
 def rna(dna):
 
     RNA = dna.replace("T", "U")
 
     return RNA[::-1]
+
+print(rna("CCTAGGACCAGGTT"))
