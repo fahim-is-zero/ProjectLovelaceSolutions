4e6051cc613d6c8dc0f2a77b3cd95f0552996042
Fahim Foysal <143939976+fahim-is-zero@users.noreply.github.com>
2024-09-25 06:41:22 +0600

    Update 15-Chaos.py

--- Python/15-Chaos.py
+++ Python/15-Chaos.py
@@ -1,10 +1,15 @@
+# Input: The parameter r.
+# Output: A list containing the first 51 values of the logistic map (x_0, x_1, x_2,......x_49, x_50).
+
+
 def logistic_map(r):
     x = [0.5]
     count = 0
 
     while count <= 49:
-        val = r * x[-1] * (1 - x[-1])
+        val = r * x[-1] * (1 - x[-1])  # Formula : x_n+1 = r * x_n(1 - x_n)
         x.append(val)
         count += 1
 
     return x
+
